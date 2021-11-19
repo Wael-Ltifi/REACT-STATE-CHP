4024bd2823b34f5db5230a4bf2d92158a10c896f
wael-ltifi <waelltifi@gmail.com>
2021-11-19 02:12:58 +0100

    react-state-chp

---
+++ src/info.js
@@ -0,0 +1,13 @@
+import image from "./ghoul.jpg";
+
+const info = {
+    imgSrc:image ,
+    fullName : "Wael Ltifi",
+    age : 29 ,
+    bio:"Wael Ltifi , A.K.A ''Big.W'' , certified beatiful male specimen, is notorious lady seducer, wanter in almost every city in the world , except in his remote hometown of sidibouzid , he's also well known for wasting people's time with trivial s**t like this ... (pay no attention to the timer above , it's only counting the number of seconds you wasted from your worthless lowly life while reading this) peace !  ",
+    profession:"None of your business !",
+    
+  }
+
+
+  export default info;
